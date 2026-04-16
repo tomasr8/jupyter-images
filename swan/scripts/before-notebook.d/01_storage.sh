5734d8b99fe3749c1bd72288031cff8a46c03f0b
Tomas Roun <tomas.roun8@gmail.com>
2026-04-16 15:24:38 +0200

    Do not create folders which are not needed

--- swan/scripts/before-notebook.d/01_storage.sh
+++ swan/scripts/before-notebook.d/01_storage.sh
@@ -38,7 +38,10 @@ then
     _log "Updated the user home to $HOME."
 fi
 
-# Make sure the user has the SWAN_projects folder
-run_as_user mkdir -p $HOME/SWAN_projects
+# Make sure the user has the SWAN_projects folder (Only if EOS_ENABLED==true)
+if [ "$EOS_ENABLED" == "true" ]
+then
+    run_as_user mkdir -p $HOME/SWAN_projects
+fi
 
 _log "Finished setting up user session storage"
